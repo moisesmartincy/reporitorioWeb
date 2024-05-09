59540d346d6a2f5b2725eb2e7f29b549f375ba8c
moisesmartincy <moisesmartincy@gmail.com>
2024-05-08 22:42:18 -0400

    nuevo cambios

--- js/index.js
+++ js/index.js
@@ -1,29 +1,31 @@
-const images = document.querySelectorAll('.image');
-const modal = document.getElementById('modal');
-const modalDescription = document.getElementById('modal-description');
+document.addEventListener("DOMContentLoaded", function() {
+    const images = document.querySelectorAll('.image');
+    const modal = document.getElementById('modal');
+    const modalDescription = document.getElementById('modal-description');
 
-images.forEach(image => {
-    image.addEventListener('click',() => {
-        const description = image.dataset.description;
-        showModal(description);
+    images.forEach(image => {
+        image.addEventListener('click',() => {
+            const description = image.dataset.description;
+            showModal(description);
+        });
     });
-});
 
-document.querySelector('.close').addEventListener('click',() => {
-    closeModal();
-});
-
-window.addEventListener('click' , (event) =>{
-    if(event.target === modal){
+    document.querySelector('.close').addEventListener('click',() => {
         closeModal();
+    });
+
+    window.addEventListener('click' , (event) =>{
+        if(event.target === modal){
+            closeModal();
+        }
+    });
+
+    function showModal(description) {
+        modalDescription.textContent = description;
+        modal.style.display = 'block';
+    }
+
+    function closeModal() {
+        modal.style.display = 'none';
     }
 });
-
-function showModal(description) {
-    modalDescription.textContent = description;
-    modal.style.display = 'block';
-}
-
-function closeModal() {
-    modal.style.display = 'none';
-}
